9a357e30452fffbb99a5c0e2c7337c1b4e3746a5
manfromexistence04 Emon <manfromexistence04@gmail.com>
2024-11-02 13:07:40 +0000

    feat: changes of plans use shadcn ui

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -3,7 +3,8 @@ import { SiteHeader } from "@/components/site-header";
 export default function Home() {
   return (
     <div>
-      <SiteHeader />
+      {/* <SiteHeader /> */}
+      Hello
     </div>
   );
 }
