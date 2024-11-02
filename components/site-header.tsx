6374b2c2d743b29af064d00a570289527d1968a8
manfromexistence04 Emon <manfromexistence04@gmail.com>
2024-11-02 07:34:49 +0000

    feat: hi

---
+++ components/site-header.tsx
@@ -0,0 +1,7 @@
+export function SiteHeader() {
+  return (
+    <header className="sticky top-0 z-50 w-full border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    Header
+    </header>
+  )
+}
